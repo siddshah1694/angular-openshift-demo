397e90f5d8ec5a045ca2036797926f25b41ea97a
siddshah1694 <36239874+siddshah1694@users.noreply.github.com>
2020-02-29 14:39:31 +0530

    added demo app code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo'`, () => {
+  it(`should have as title 'angular-openshift-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo');
+    expect(app.title).toEqual('angular-openshift-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-openshift-demo app is running!');
   });
 });
